4f8c29c62479e4c15b28d7185d93d055e7246412
Casey Sweet <sweetca72@gmail.com>
2020-06-25 21:15:50 -0700

    Transferred rand num gen to new files

---
+++ app/src/main/java/com/example/basicrandomnumberapp/randNumGen.java
@@ -0,0 +1,14 @@
+package com.example.basicrandomnumberapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class randNumGen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rand_num_gen);
+    }
+}
